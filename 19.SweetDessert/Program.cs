d5187eb19b1795050131b7ba960fce3159f3ebbf
sisisi11 <everton.sky.1231@gmail.com>
2016-09-26 09:38:46 +0300

    Update

---
+++ 19.SweetDessert/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.SweetDessert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2 bananas, 4 eggs and 0.2 kilos berries == set of 6 ; 1 dessert = set of 6 ; 10 peoples = set of 12 (2 dessert) ; 5 pepole = set of 6 (1 dessert).
+
+            float Cash = float.Parse(Console.ReadLine());
+            int Peoples = int.Parse(Console.ReadLine());
+            float banana = float.Parse(Console.ReadLine());
+            float egg = float.Parse(Console.ReadLine());
+            float berry = float.Parse(Console.ReadLine());
+
+            int DessertCount = 0;
+
+            do
+            {
+                DessertCount=0;
+            }
+            while (Peoples % 6 == 0);
+            {
+                DessertCount++;
+                
+            }
+            Console.WriteLine(Cash);
+        }
+    }
+}
---
+++ 20.Photographer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_1_.Photographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ammountPhoto = int.Parse(Console.ReadLine());
+            var ammountTimeRequire = int.Parse(Console.ReadLine());
+            var PrecentGoods = int.Parse(Console.ReadLine()); ;
+            long ammountTimeUp = long.Parse(Console.ReadLine());
+
+            var totalGoodPhoto = (int)Math.Ceiling((ammountPhoto * PrecentGoods) / 100.0);
+
+
+
+            long ammountofALL = ammountTimeRequire * ammountPhoto; // in SECS
+            long ammountofGod = totalGoodPhoto * ammountTimeUp; //in SECS
+
+            long totalTime = ammountofALL + ammountofGod; // in secs
+
+            long days = totalTime / 86400;
+            long daysLeft = totalTime % 86400;
+            long hours = daysLeft / 3600;
+            long hoursLeft = daysLeft % 3600;
+            long min = hoursLeft / 60;
+            long sec = hoursLeft % 60;
+
+
+            Console.WriteLine("{0}:{1:D2}:{2:D2}:{3:D2}", days, hours, min, sec);
+        }
+    }
+}
